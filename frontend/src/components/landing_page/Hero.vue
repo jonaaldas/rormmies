288a81380c8719d546a31296e4cb733cf18e7899
jonaaldas <jonaaldas@gmail.com>
2023-12-07 21:24:40 -0500

    change folder name

---
+++ frontend/src/components/landing_page/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="py-8 lg:py-20" id="home">
+        <div class="container">
+            <div class="grid gap-12 lg:grid-cols-2">
+                <div>
+                    <h1 class="text-4xl font-black tracking-tighter lg:text-6xl lg:leading-none">Discover Short-Term and Flexible Nurseries, Daycares, Preschools, and Drop-in Programs Worldwide</h1>
+                    <p class="mt-8 text-lg">
+                        Traveling freely with your little one(s) has always been the goal. But, once you get them into daycare or preschool, it’s hard to walk away. Are you willing to pull them out so
+                        you can hit the road again? What if you didn’t have to choose one or the other?
+                    </p>
+                    <div class="mt-16 inline-flex gap-3">
+                        <div class="join w-full">
+                            <input class="input input-bordered join-item" placeholder="Email" />
+                            <button class="btn join-item rounded-r-full">Join Now</button>
+                        </div>
+                        <button class="btn btn-primary">Lear More</button>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="rounded-2xl bg-gradient-to-r from-indigo-200 via-red-200 to-purple-300 p-3" data-card3d="" data-card3d-perspective="1000" data-card3d-full-page-listening="true">
+                        <img src="../../assets/img/jessica-rockowitz-5NLCaz2wJXE-unsplash.jpg" alt="" class="rounded-2xl" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ frontend/src/components/landing_page/Trifold.vue
@@ -0,0 +1,121 @@
+<template>
+    <section class="relative py-8 lg:py-20" id="features">
+        <div class="absolute start-[10%] z-0">
+            <div
+                class="pointer-events-none aspect-square w-60 rounded-full bg-gradient-to-r from-primary/10 via-violet-500/10 to-purple-500/10 blur-3xl [transform:translate3d(0,0,0)] lg:w-[600px]"
+            ></div>
+        </div>
+
+        <div class="container">
+            <div class="flex flex-col items-center">
+                <h2 class="inline text-4xl font-semibold">👋 Who We Are</h2>
+
+                <p class="mt-4 text-lg sm:text-center">
+                    Hey, there! We’re new(ish) parents to a curious 19-month year old who loves to explore. Before having a child, we lived abroad and traveled the world for 10 years. We always knew
+                    once we’d have a kid that we wanted to show her the world. But, we were in for a real surprise when she was born. The sleep deprivation and challenges of caring for a newborn while
+                    managing work-from-home with no help led us to put that traveling lifestyle on the backburner, at least for a while. So, we settled for moving back abroad to Portugal so we could
+                    still have a taste of the abroad life, but still have a chance to get some of our time back.
+                </p>
+            </div>
+
+            <div class="relative z-[2] mt-8 grid gap-8 lg:mt-20 lg:grid-cols-2 lg:gap-12">
+                <div class="overflow-hidden rounded-lg bg-base-200 shadow-md transition-all hover:shadow-xl">
+                    <img alt="saas img" class="overflow-hidden rounded-ss-lg" src="../../assets/img/daycare.jpg" />
+                </div>
+
+                <div class="lg:mt-8">
+                    <h1 class="mt-2 text-xl text-center font-semibold">🌍 The Game-Changer: When Your Kid Starts Daycare</h1>
+                    <p class="mt-2 text-center font-medium">
+                        When we finally got our daughter into “creche” at 16-months old, she absolutely LOVED the routine, activities, healthy lunches, and socialization that it offered her. She’s
+                        there right now!
+                    </p>
+                    <p class="mt-2 text-center font-medium">And, it changed our lives, too.</p>
+                    <p class="mt-2 text-center font-medium">
+                        So much so, that even though the travel devil on our shoulder is whispering in our ears to go out and hit the road again, the angel on the other shoulder whispers back.
+                    </p>
+                    <p class="mt-2 text-center font-medium">“Can we really take her out of school now?”</p>
+                    <p class="mt-2 text-center font-medium">
+                        Wait. Really? How ironic. The exact 9-5 system we’ve escaped becoming digital nomads is exactly what’s serving us and our child—and us—right now.
+                    </p>
+                    <p class="mt-2 text-center font-medium">
+                        Let’s be real. Traveling with kids is just parenting in another city. You dream of hitting a restaurant in the middle of the day, but the playground is the only chance for you
+                        to relax and your kid to play.
+                    </p>
+                    <p class="mt-2 text-center font-medium">
+                        This is why having daycare available is an absolute necessity for digital nomad families with young children. They have fun and routine, you have a much-needed break to
+                        actually explore the place you’re in.
+                    </p>
+                </div>
+            </div>
+
+            <div class="mt-8 grid gap-8 lg:mt-20 lg:grid-cols-2 lg:gap-12">
+                <div>
+                    <h3 class="mt-2 text-3xl font-semibold">🔍 The Problem: Finding Short-Term & Flexible Options for 0-6</h3>
+                    <p class="mt-2 text-center">
+                        Sure, there are plenty of school options for traveling families, many of which we plan to take advantage of once our daughter is old enough. This is because in general,
+                        daycares and preschools require a long-term commitment, as well as a transition period for them to get comfortable with the new routine.
+                    </p>
+                    <p class="mt-2 text-center">So, if daycare isn’t available, parents are left making a difficult decision.</p>
+
+                    <ul class="mt-4 list-inside text-center text-base">
+                        <li>Do you pull them out?</li>
+                        <li>Does one parent quit their job to take care of the child full-time?</li>
+                        <li>What if that parent needs a break?</li>
+                        <li>ANd, what about single parents?</li>
+                    </ul>
+                    <p class="mt-2 text-center">And, what about the child? Do they even want to be with mom and dad all day? Will traveling be even worth it if you’re constantly stressed?</p>
+                    <p class="mt-2 text-center font-semibold">Traveling families need options.</p>
+                    <p class="mt-2 text-center">
+                        The thing is,, when we started looking, we realized it’s extremely challenging to find information on schools that are not only available for children 0-6 years old, but also
+                        offer short-term and flexible enrollment for digital nomad families who may want to explore a place for just a few weeks or months at a time. There are so many questions to ask
+                        during this process, and that’s on top of trip planning!
+                    </p>
+                    <p class="mt-2 text-center font-semibold">Who has time for that?!</p>
+                </div>
+
+                <div class="order-first lg:order-last">
+                    <div class="overflow-hidden rounded-lg bg-base-200 shadow-md transition-all hover:shadow-xl">
+                        <img alt="saas img" class="overflow-hidden rounded-ss-lg" src="../../assets/img/kid ipad.jpg" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex flex-col items-center mt-16">
+                <h2 class="inline text-4xl font-semibold">🚀 Our Mission: We’re Doing the Research for You, So You Can Focus on Everything Else</h2>
+
+                <p class="mt-4 text-lg sm:text-center">
+                    What if we made it easier for digital nomad parents to find exactly what they need for their busy babies, active toddlers, and pre-kindys, wherever they want to go in the world?
+                </p>
+                <p class="mt-4 text-lg sm:text-center">
+                    From crowdsourcing, personal recommendations, word-of-mouth, online forums, and talking to schools directly, we’re building our database from the ground up.
+                </p>
+
+                <p class="mt-4 text-lg sm:text-center font-semibold">Best of all, our database is curated by parents, for parents.</p>
+                <p class="mt-4 text-lg sm:text-center">
+                    We are making sure to vet each school and program to meet a certain set of expectations and standards that we have for our own child. And, as former teachers, we also understand
+                    the other side. We find that these values are shared by many digital nomad families.
+                </p>
+                <p class="mt-4 text-lg sm:text-center font-semibold">Most importantly, we’re giving you (some) of your time back.</p>
+            </div>
+
+            <div class="flex flex-col items-center mt-16">
+                <h2 class="inline text-4xl font-semibold">Our Goals…</h2>
+
+                <p class="mt-4 text-lg sm:text-center">✓ Find schools that are open to international families, even if you don’t speak/read the local language.</p>
+                <p class="mt-4 text-lg sm:text-center">✓ Find schools that offer short-term and flexible options for weeks or months at a time.</p>
+                <p class="mt-4 text-lg sm:text-center">✓ Find schools that have easy sign-up options, so you can guarantee a spot for your child before booking your trip.</p>
+
+                <p class="mt-4 text-lg sm:text-center">❌ We’re not including nannies or childcare options.</p>
+                <p class="mt-4 text-lg sm:text-center">❌We don’t want to disrupt the dynamic in the classroom, so the school must have minimum attendance expectations.</p>
+                <p class="mt-4 text-lg sm:text-center">
+                    ❌We’re not creating another digital nomad bubble. We are aiming to include as many LOCAL daycares as possible, so monolingual schools are not off the list. (Our daughter goes to a
+                    fully-Portuguese daycare.)
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script></script>
+
+<style></style>
