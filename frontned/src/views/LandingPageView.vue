c1916e71c967651d30ab0d40fd54b98da5603c45
jonaaldas <jonaaldas@gmail.com>
2023-12-07 21:15:39 -0500

    starter

---
+++ frontned/src/views/LandingPageView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="overflow-x-hidden bg-base-100 font-body text-sm text-base-content antialiased">
+        <NavBar />
+        <Hero />
+        <Trifold />
+    </div>
+</template>
+
+<script>
+    import Hero from '../components/landing_page/Hero.vue';
+    import NavBar from '../components/landing_page/NavBar.vue';
+    import Trifold from '../components/landing_page/Trifold.vue';
+    export default {
+        components: {Hero, NavBar, Trifold},
+    };
+</script>
+
+<style></style>
+``
