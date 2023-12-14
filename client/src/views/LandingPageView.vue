8030cc394c6f59af80efd66bbca4d6673b37c83d
jonaaldas <jonaaldas@gmail.com>
2023-12-13 21:46:51 -0500

    changed folder name

---
+++ client/src/views/LandingPageView.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="overflow-x-hidden bg-base-100 font-body text-sm text-base-content antialiased">
+		<NavBar />
+		<Hero />
+		<Trifold />
+		<Pricing />
+	</div>
+</template>
+
+<script>
+	import Hero from '../components/landing_page/Hero.vue';
+	import NavBar from '../components/landing_page/NavBar.vue';
+	import Trifold from '../components/landing_page/Trifold.vue';
+	import Pricing from '../components/landing_page/Pricing.vue';
+	export default {
+		components: {Hero, NavBar, Trifold, Pricing}
+	};
+</script>
+
+<style></style>
